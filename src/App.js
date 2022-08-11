1d6549dfb15ab6923cef4c7a0da8d6d88252cbb4
Eric <10673424+estraw1059@users.noreply.github.com>
2022-08-11 08:26:25 -0500

    Basic Nav bar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import './App.css';
 function App() {
   return (
-    <div className="App">
-      Hi Caroline
+    <div>
+      <nav>
+        <ul className="menu">
+          <li><a href="/">Home</a></li>
+          <li><a href="/resume">Resume</a></li>
+          <li><a href="/blogPost">Blog Post</a></li>
+          <li><a href="/github">Github</a></li>
+          <li><a href="/linkedin">Linkedin</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
